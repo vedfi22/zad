6c912d14b0d7c7401b9bb5eee999874df08f08a6
vedfi22 <sasha.sharipov888@gmail.com>
2025-07-17 16:27:20 +0300

    Add files via upload

---
+++ моб прил/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'smart-todo-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
